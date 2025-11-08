38290f229e9c951700a7df17838eb9c48ff9b17d
pista <pyh5523@gmail.com>
2025-11-09 00:41:00 +0900

    test: fixed calss_main

--- lexer/src/tests.rs
+++ lexer/src/tests.rs
@@ -1,5 +1,5 @@
 use crate::lexer::Lexer;
-use crate::lexer::*;
+use crate::lexer::TokenType;
 
 #[cfg(test)]
 mod tests{
@@ -10,13 +10,12 @@ mod tests{
     fn test_class_main(){
         let source_1="public class Main { }";
         let mut lexer = Lexer::new(source_1);
-        let toknes = lexer.tokenize();
+        let tokens = lexer.tokenize();
 
         assert_eq!(tokens[0].token_type, TokenType::Public);
-        assert_eq!(tokens[0].token_type, TokenType::Public);
-        assert_eq!(tokens[0].token_type, TokenType::Public);
-        assert_eq!(tokens[0].token_type, TokenType::Public);
-        assert_eq!(tokens[0].token_type, TokenType::Public);
-
+        assert_eq!(tokens[1].token_type, TokenType::Class);
+        assert_eq!(tokens[2].token_type, TokenType::Identifier("Main".to_owned()));
+        assert_eq!(tokens[3].token_type, TokenType::LeftBrace);
+        assert_eq!(tokens[4].token_type, TokenType::RightBrace);
     }
 }
